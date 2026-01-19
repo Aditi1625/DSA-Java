34dfe005e06ee1168d7a11e8b87b2f585203f744
Aditi Kagwade <kagwadeaditi@gmail.com>
2026-01-19 21:55:56 +0530

    Day 01: Added basic Java program (Sum of Two Numbers)

---
+++ 01_Arrays/TwoSum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class First{
+public static void main(String[]args){
+    System.out.print("enter first number ");
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+System.out.print("enter second number ");
+int b=sc.nextInt();
+int sum=a+b;
+System.out.print("the sum of entered number is "+sum);
+
+
+}
+
+}
