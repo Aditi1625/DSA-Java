f26720e89fbca7107e7ebe75e89ff7ea58b8496b
Aditi Kagwade <kagwadeaditi@gmail.com>
2026-01-21 21:14:32 +0530

    Day 02:Find maximum Element

---
+++ 01_Arrays/MaxElement.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class maximun{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter 1st number of ur choice-");
+        int a=sc.nextInt();
+        System.out.print("enter 2nd number of ur choice-");
+        int b=sc.nextInt();
+
+        if(a>b){
+            System.out.print("1st number is greater than 2nd number");
+
+        }
+        else{
+            System.out.print("2nd number is greater than 1st number");
+        }
+    }}
+
