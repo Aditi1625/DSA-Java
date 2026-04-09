2cff8bed76ada75f479e9e018fece72820d98ef6
Aditi kagwade <kagwadeaditi@gmail.com>
2026-04-09 19:56:06 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (99.78%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n; // handle overflow
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1;
+
+        while (N > 0) {
+            // If n is odd
+            if (N % 2 == 1) {
+                result *= x;
+            }
+
+            // Square the base
+            x *= x;
+
+            // Divide exponent by 2
+            N /= 2;
+        }
+
+        return result;
+    }
+}
