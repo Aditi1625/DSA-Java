abdbd9e81058280db2a8e8cee73711d95dd35fe7
Aditi kagwade <kagwadeaditi@gmail.com>
2026-07-07 12:02:07 +0530

    Time: 4 ms (67.09%) | Memory: 94.1 MB (94.25%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // 1. Find the middle of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 2. Reverse the second half of the linked list
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // 3. Compare the first and second halves
+        ListNode left = head;
+        ListNode right = prev; // Head of the reversed second half
+        while (right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
