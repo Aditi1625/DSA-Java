eab67960316f085fe647ad61168760b8de5df603
Aditi kagwade <kagwadeaditi@gmail.com>
2026-07-28 22:16:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (34.58%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode current = root;
+        
+        while (current != null) {
+            
+            if (current.left != null) {
+                
+                TreeNode rightmost = current.left;
+                while (rightmost.right != null) {
+                    rightmost = rightmost.right;
+                }
+                
+                
+                rightmost.right = current.right;
+                
+                
+                current.right = current.left;
+                current.left = null; 
+            }
+            
+            
+            current = current.right;
+        }
+    }
+}
